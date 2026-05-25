f8f0946a13d818f5920a1abc22ff097c8cf8905a
GUILDER <pplg39395@gmail.com>
2026-05-25 08:50:47 -0400

    Conexion con fuseky y ya no con local o el archivo rdf

---
+++ src/config/fuseki.js
@@ -0,0 +1,7 @@
+module.exports = {
+  endpoint: process.env.FUSEKI_QUERY_ENDPOINT || 'http://localhost:3030/calidadsoft/query',
+  defaultQueryOptions: {
+    format: 'application/sparql-results+json',
+    timeout: 30000
+  }
+};
